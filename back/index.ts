3d6f06d93c60977607dc435b9943551e0a310cf2
zero <theloostt@live.com>
2023-02-09 10:18:04 -0300

    --amend

--- back/index.ts
+++ back/index.ts
@@ -11,7 +11,6 @@ const app = express();
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb' ,extended: true}));
 app.use(cors());
-app.use(express.static('dist'))
 
 app.get('/', (req, res) => {
   res.sendFile(join(__dirname, 'dist', 'index.html'));
