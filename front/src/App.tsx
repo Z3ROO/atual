fbe1531af3a1b4dcd8b5b18f2d617803300c998b
zero <theloostt@live.com>
2023-02-08 23:45:07 -0300

    feat: insert folder icon

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,9 +1,11 @@
 import { Editor } from './features/Editor';
+import folderIcon from './assets/folder.svg';
 
 function App() {
   return (
     <div className="App">
       <div className="top-bar">
+        <img src={folderIcon} />
         <span className='top-bar-span'>Site</span>
         <span className='top-bar-divisor'>/</span>
         <span className='top-bar-span'>Categoria</span>
