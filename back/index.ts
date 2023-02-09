ff362540a66dd3b79a93577f0dc63ad877d280f5
zero <theloostt@live.com>
2023-02-09 08:40:48 -0300

    fix: port by env var

--- back/index.ts
+++ back/index.ts
@@ -66,6 +66,6 @@ app.post('/image', (req, res) => {
 });
 
 
-app.listen(3000, () => {
-  console.log('Server connected on 3000');
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Server connected');
 })
